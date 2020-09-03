797f4def80ca9311f6c4daac1c65814c28b937f6
schari2509 <schari2509@gmail.com>
2020-09-03 15:27:51 +0530

    added secondary actvities

---
+++ app/src/main/java/com/schari/tutmanager/EnquiryActivity.java
@@ -0,0 +1,14 @@
+package com.schari.tutmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnquiryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enquiry);
+    }
+}
--- app/src/main/java/com/schari/tutmanager/MainActivity.java
+++ app/src/main/java/com/schari/tutmanager/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +30,33 @@ public class MainActivity extends AppCompatActivity {
         feesCard = findViewById(R.id.fees_card);
         studentsCard = findViewById(R.id.students_card);
         enquiryCard = findViewById(R.id.enquiries_card);
+
+        attendanceCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AttendanceActivity.class));
+            }
+        });
+
+        feesCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FeeActivity.class));
+            }
+        });
+
+        enquiryCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EnquiryActivity.class));
+            }
+        });
+
+        studentsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StudentActivity.class));
+            }
+        });
     }
 }
